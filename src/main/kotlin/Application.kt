5b46f3e4d78f76bfcb007bc7807c0ca4c3aa9dc4
Daazay <41193822+Daazay@users.noreply.github.com>
2025-04-14 10:12:54 +0300

    Initial commit.

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,16 @@
+package org.daazay
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSecurity()
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureRouting()
+}
